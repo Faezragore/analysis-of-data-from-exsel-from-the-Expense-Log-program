1297e18416c25b34e3fe6cca16e86ebac8a581ac
Faezragore <e.v.tarasevich@yandex.ru>
2022-03-09 01:22:37 +0300

    added the selection of a specific month

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import pandas
 import xlrd
 import collections
 import datetime
+import sys
+import os
+import argparse
 from pandas._libs.tslibs.timestamps import Timestamp
 
 
@@ -18,25 +21,35 @@ second_column_in_the_file_exel = product_categories_in_the_exel_file.columns.rav
 third_column_in_the_file_exel = product_categories_in_the_exel_file.columns.ravel()[2]
 
 
-def get_all_product_positions(position):  #получить все позиции продукта
+def choose_a_month_or_a_year (): # Создаем экземпляр класса ArgumentParser
+    month_or_year = argparse.ArgumentParser()
+    month_or_year.add_argument ('--choosing_month', default=['year'], help='your choice')
+
+    return month_or_year
+
+def get_all_product_positions(position):  # получить все позиции продукта
     total_amount = 0
     for one_product_position in all_product_position:
          one_product_position[first_column_in_the_file_exel] = str(one_product_position.get(first_column_in_the_file_exel)).split()[0]
          if position in one_product_position[second_column_in_the_file_exel]:
-             if '01' in one_product_position[first_column_in_the_file_exel][5:7]:
+             if selected_date in one_product_position[first_column_in_the_file_exel][5:7]:
                 total_amount += one_product_position[third_column_in_the_file_exel]
+    
     group_of_products[position].append(total_amount)
 
 
-def get_a_position_from_a_list_of_products(list_of_product_categories): #получить_позицию из_списка_ категорий продуктов
+def get_a_position_from_a_list_of_products(list_of_product_categories): # получить_позицию из_списка_ категорий продуктов
     for position in list_of_product_categories:
         get_all_product_positions(position)
 
 
-def show_monthly_expense(group_of_products): #показать_месячные_расходы
+def show_expenses_for_year(group_of_products): # показать расходы за год
+    print("расходов нету")
+
+def show_monthly_expense(group_of_products): # показать месячные расходы
     get_a_position_from_a_list_of_products(list_of_product_categories)
     total_amount = 0    #итоговая сумма
-    sorted_product_group_dictionary = sorted(group_of_products.items(), key=lambda x: x[1])  #отсортированный_словарь группы продуктов
+    sorted_product_group_dictionary = sorted(group_of_products.items(), key=lambda x: x[1])  # отсортированный словарь группы продуктов
     for product_category, amount in dict(sorted_product_group_dictionary).items():
         total_amount += amount[0]
         print("За месяц на", product_category, "потрачено", amount[0], "рублей!")
@@ -44,4 +57,10 @@ def show_monthly_expense(group_of_products): #показать_месячные_
 
 
 if __name__ == "__main__":
-    show_monthly_expense(group_of_products)
+    month_or_year = choose_a_month_or_a_year()
+    namespase = month_or_year.parse_args(sys.argv[1:])
+    selected_date = namespase.choosing_month # Записываем в переменную выбор месяца или за весь год
+    if selected_date == "year":
+        show_expenses_for_year(group_of_products)
+    else:
+        show_monthly_expense(group_of_products)
