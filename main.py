3eb99f6c88625cc5a9bfacfe848189de3f7ac22a
Faezragore <30468128+Faezragore@users.noreply.github.com>
2022-07-08 08:09:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -102,3 +102,98 @@ if __name__ == "__main__":
         show_monthly_expense(group_of_products, str(selected_year), str(choosing_month))
     elif choice == "3":
         pass
+    
+    
+    ******************************
+    #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import pandas
+import xlrd
+import collections
+import datetime
+import sys
+import os
+import argparse
+from pandas._libs.tslibs.timestamps import Timestamp
+from rich.console import Console
+from rich.table import Table
+
+
+list_of_product_categories = ['Еда', 'Хлеб (батон)', 'Фрукты', 'Овощи', 'Крупы,макароны', 'Печенье(конфеты и другое сладкое)', 'Молочка(молоко,кефир,творог)', 'Мясо(кура,гов,свинина,индейка)', 'Пюре,йогурт детям.', 'Ветчина(колбаса,сосиски)', 'Вкусности детям', 'Работа_еда']
+product_categories_in_the_exel_file = pandas.read_excel('Report.xls', sheet_name='Report', keep_default_na=False)
+all_product_position = product_categories_in_the_exel_file.to_dict(orient='record')
+group_of_products = collections.defaultdict(list)
+first_column_in_the_file_exel = product_categories_in_the_exel_file.columns.ravel()[0]
+second_column_in_the_file_exel = product_categories_in_the_exel_file.columns.ravel()[1]
+third_column_in_the_file_exel = product_categories_in_the_exel_file.columns.ravel()[2]
+total_amount_for_product_category_year = collections.defaultdict(list)
+
+
+def get_all_product_positions(position, selected_year, choosing_month): # получить все позиции продукта
+    #print(selected_year)
+    total_amount = 0
+    for one_product_position in all_product_position:
+        #print(one_product_position)
+        date_comparison = str(one_product_position['DateTime'])
+        if position in one_product_position[second_column_in_the_file_exel]:
+            if selected_year in date_comparison:
+                #print(selected_year)
+                if choosing_month == "False":
+                    total_amount += -1 * one_product_position[third_column_in_the_file_exel]
+                else:
+                    date_comparison = date_comparison[5:7]
+                    if choosing_month == date_comparison:
+                        total_amount += -1 * one_product_position[third_column_in_the_file_exel]
+
+    group_of_products[position].append(total_amount)
+
+
+def get_a_position_from_a_list_of_products(list_of_product_categories, selected_year, choosing_month): # получить_позицию из_списка_ категорий продуктов
+    #print(selected_year)
+    group_of_products.clear()
+    for position in list_of_product_categories:
+        #print(selected_year)
+        #print(type(choosing_month))
+        get_all_product_positions(position, selected_year, choosing_month)
+
+
+def count_monthly_expenses_for_a_year(group_of_products, selected_year): # считать месячные расходы для года
+    list_of_months = ['01','02','03','04','05','06','07','08','09','10','11','12']
+    for choosing_month in list_of_months:
+        selected_year = selected_year
+        get_a_position_from_a_list_of_products(list_of_product_categories, selected_year, choosing_month)
+        total_amount = 0    #итоговая сумма
+        sorted_product_group_dictionary = sorted(group_of_products.items(), key=lambda x: x[1])  # отсортированный словарь группы продуктов
+        for product_category, amount in dict(sorted_product_group_dictionary).items():
+            total_amount += amount[0]
+            print(product_category , round(amount[0]))
+                #print(total_amount)
+        print("Итого за месяц ", choosing_month, "года ", selected_year,   "на питание потрачено:", round(total_amount), "рублей!")
+
+def count_for_all_time_in_file(selected_year):
+    count_monthly_expenses_for_a_year(group_of_products, selected_year)
+
+if __name__ == "__main__":
+    list_of_years = ['2020','2021','2022']
+    for selected_year in list_of_years:
+        count_for_all_time_in_file(selected_year)
+
+bank_db = {
+    'year' : ['2021']
+    'product_categories' : ['Еда'],
+    'январь' : [],
+    'февраль' : [],
+    'март' : [],
+    'апрель' : [],
+    'январь' : [],
+    'февраль' : [],
+    'март' : [],
+    'апрель' : [],
+    'январь' : [],
+    'февраль' : [],
+    'март' : [],
+    'апрель' : [],
+}
+
+
